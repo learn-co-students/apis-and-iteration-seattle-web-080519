27bb1087f77174b558afecc18e74902b82351cc4
github username <github email address>
2019-08-19 16:34:09 -0700

    HAIL BANE

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,12 +1,31 @@
 require 'rest-client'
 require 'json'
 require 'pry'
-
+$v = ""
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://www.swapi.co/api/people/')
+  response_string = RestClient.get("http://www.swapi.co/api/people/?search=#{character_name}")
   response_hash = JSON.parse(response_string)
-
+  # binding.pry
+  if response_hash["count"] == 0
+    $v = "bleh"
+    puts "no name exist in the star wars universe try again"
+  else
+    return response_hash["results"][0]["films"].map {|e| JSON.parse(RestClient.get(e))}
+  end
+ 
+  #  p response_hash.map do |k,v|
+  #     if v.class == Array
+  #       v.each do |e|
+  #         if e["name"] == character_name
+  #           e["films"].each do |film|
+  #             films << film
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+    # films
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -19,12 +38,18 @@ def get_character_movies_from_api(character_name)
 end
 
 def print_movies(films)
+  if $v == "bleh"
+    puts "please input a starwars character name"
+  else
+  films.each {|e| puts e["title"]} 
+  end
   # some iteration magic and puts out the movies in a nice list
 end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_movies(films)
+  # binding.pryd
 end
 
 ## BONUS
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
-  # puts out a welcome message here!
+  puts "HAIL BANE"
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  char = gets.chomp
+  p char
   # use gets to capture the user's input. This method should return that input, downcased.
 end
